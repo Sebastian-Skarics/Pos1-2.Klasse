bcb30f837c182faf004af062019ab554e28c627f
Sebastian-Skarics <sebastian.skarics@gmx.at>
2020-03-03 16:41:01 +0100

    14

--- Pos1/simpleRPN/src/main.cpp
+++ Pos1/simpleRPN/src/main.cpp
@@ -4,17 +4,24 @@
 #include <iostream>
 #include <string>
 #include <exception>
+#include <sstream>
 
 using namespace std;
 
 
-double rechnung(char op, int r1, int r2){
+double rechnung(char op, string r1, string r2){
   double erg {};
-  switch(x[0]){
+  switch(op){
     case '+': erg = stod(r2) + stod(r1); break;
     case '-': erg = stod(r2) - stod(r1); break;
     case '*': erg = stod(r2) * stod(r1); break;
-    case '/': erg = stod(r2) / stod(r1); break;
+    case '/':
+    if(r1 == "0"){
+      throw invalid_argument("Division by 0");
+    }
+    else{
+      erg = stod(r2) / stod(r1); break;
+    }
     default: throw invalid_argument("Operator entspricht nicht den Vorraussetzungen");
   }
   return erg;
@@ -29,28 +36,37 @@ void registerSwitch(string &r1, string &r2, string &r3, string &r4){
 }
 
 
+void registerSwitchRev(string &r1, string &r2, string &r3, string &r4){
+  r2 = r3;
+  r3 = r4;
+  r4 = "0";
+}
+
+
 int main(){
-  string r4 {};
-  string r3 {};
-  string r2 {};
-  string r1 {};
+  string r4 {"0"};
+  string r3 {"0"};
+  string r2 {"0"};
+  string r1 {"0"};
   string nInp {};
   while(true){
-    double erg {};
+    ostringstream strs {};
+    double erg {stod(r2)};
     cin >> nInp;
-    if(nInp[0] == '+' || nInp[0] == '-' || nInp[0] == '*' || nInp[0] == '/''){
+    if(nInp[0] == '+' || nInp[0] == '-' || nInp[0] == '*' || nInp[0] == '/'){
       try{
-        erg = rechnung(x, r1, r2);
+        erg = rechnung(nInp[0], r1, r2);
+        registerSwitchRev(r1, r2, r3, r4);
+        strs << erg;
+        r1 = strs.str();
+        cout << r1 << endl;
       }
       catch(exception &e){
-        cout << "Es ist zu einem Fehler gekommen -->" << e.what() << endl;
+        cout << e.what() << endl;
       }
-      registerSwitch();
-      r1 = std(erg)
-      cout << r1;
     }
     else{
-      registerSwitch()
+      registerSwitch(r1, r2, r3, r4);
       r1 = nInp;
     }
   }
