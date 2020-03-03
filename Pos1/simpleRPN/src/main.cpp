b90715e30144483c7ab08e427c6c29d5ba4c357f
Sebastian-Skarics <sebastian.skarics@gmx.at>
2020-03-03 13:13:44 +0100

    12

--- Pos1/simpleRPN/src/main.cpp
+++ Pos1/simpleRPN/src/main.cpp
@@ -7,27 +7,51 @@
 
 using namespace std;
 
+
+double rechnung(char op, int r1, int r2){
+  double erg {};
+  switch(x[0]){
+    case '+': erg = stod(r2) + stod(r1); break;
+    case '-': erg = stod(r2) - stod(r1); break;
+    case '*': erg = stod(r2) * stod(r1); break;
+    case '/': erg = stod(r2) / stod(r1); break;
+    default: throw invalid_argument("Operator entspricht nicht den Vorraussetzungen");
+  }
+  return erg;
+}
+
+
+void registerSwitch(string &r1, string &r2, string &r3, string &r4){
+  r4 = r3;
+  r3 = r2;
+  r2 = r1;
+  return;
+}
+
+
 int main(){
+  string r4 {};
+  string r3 {};
   string r2 {};
   string r1 {};
-  string x {};
+  string nInp {};
   while(true){
     double erg {};
-    cin >> r2;
-    cin >> r1;
-    cin >> x;
-    try{
-      switch(x[0]){
-        case '+': erg = stod(r2) + stod(r1); break;
-        case '-': erg = stod(r2) - stod(r1); break;
-        case '*': erg = stod(r2) * stod(r1); break;
-        case '/': erg = stod(r2) / stod(r1); break;
-        default: throw invalid_argument("Operator entspricht nicht den Vorraussetzungen");
+    cin >> nInp;
+    if(nInp[0] == '+' || nInp[0] == '-' || nInp[0] == '*' || nInp[0] == '/''){
+      try{
+        erg = rechnung(x, r1, r2);
       }
-      cout << erg << endl;
+      catch(exception &e){
+        cout << "Es ist zu einem Fehler gekommen -->" << e.what() << endl;
+      }
+      registerSwitch();
+      r1 = std(erg)
+      cout << r1;
     }
-    catch(exception &e){
-      cout << "Es ist zu einem Fehler gekommen -->" << e.what() << endl;
+    else{
+      registerSwitch()
+      r1 = nInp;
     }
   }
 }
